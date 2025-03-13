177805f72ce851b2af08d41bb5fd0d3eb1c12e49
Nnange <awong.nnange@yahoo.com>
2025-03-13 20:10:08 +0100

    added functions for geting data from database

---
+++ premier-zone/premier-zone/src/main/java/com/pl/premier_zone/player/PlayerService.java
@@ -0,0 +1,52 @@
+package com.pl.premier_zone.player;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class PlayerService {
+    private final PlayerRepository playerRepository;
+
+    @Autowired
+    public  PlayerService(PlayerRepository playerRepository){
+        this.playerRepository = playerRepository;
+    }
+
+    public List<Player> getPlayer(){
+        return playerRepository.findAll();
+    }
+
+    public List<Player> getPlayersFromTeams(String teamName) {
+        return playerRepository.findAll().stream()
+                .filter(player -> teamName.equals(player.getTeam()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByName(String searchText) {
+        return playerRepository.findAll().stream()
+                .filter(player -> player.getName().toLowerCase().contains(searchText.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByPos(String searchText) {
+        return playerRepository.findAll().stream()
+                .filter(player -> player.getPos().toLowerCase().contains(searchText.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayerByNation(String searchText) {
+        return playerRepository.findAll().stream()
+                .filter(player -> player.getNation().toLowerCase().contains(searchText.toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Player> getPlayersByTeamAndPosition(String team, String position) {
+        return playerRepository.findAll().stream()
+                .filter(player -> team.equals(player.getTeam()) && position.equals(player.getPos()))
+                .collect(Collectors.toList());
+    }
+
+}
