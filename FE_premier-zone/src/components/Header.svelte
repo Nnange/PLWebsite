d747bacbf59d80f294ceb49185d9545f21281e3d
Nnange <awong.nnange@yahoo.com>
2025-03-17 22:55:56 +0100

    ongoing implementation of the header component

---
+++ FE_premier-zone/src/components/Header.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const word: string = 'Premier League';
+</script>
+
+<div class="grid grid-cols-12 content-center bg-(--league-color) text-white">
+	<img src="images/circularLogo.png" class="col-span-2 ml-5" alt="premierLeagueLogo" width="80" />
+	<div class="col-span-2 grid content-center justify-items-center">Home</div>
+	<div class="col-span-2 grid content-center justify-items-center">Team</div>
+	<div class="col-span-2 grid content-center justify-items-center">Country</div>
+	<div class="col-span-2 grid content-center justify-items-center">Player Data</div>
+</div>
