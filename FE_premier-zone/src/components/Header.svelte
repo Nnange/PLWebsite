0f67a0d433ff4cd6869850989eed59517e6681be
Nnange <awong.nnange@yahoo.com>
2025-03-18 16:15:43 +0100

    implemented the homepage

--- FE_premier-zone/src/components/Header.svelte
+++ FE_premier-zone/src/components/Header.svelte
@@ -1,11 +1,36 @@
 <script lang="ts">
+	import { Icon, Home, UserGroup, Flag, TableCells } from 'svelte-hero-icons';
+
 	const word: string = 'Premier League';
 </script>
 
 <div class="grid grid-cols-12 content-center bg-(--league-color) text-white">
 	<img src="images/circularLogo.png" class="col-span-2 ml-5" alt="premierLeagueLogo" width="80" />
-	<div class="col-span-2 grid content-center justify-items-center">Home</div>
-	<div class="col-span-2 grid content-center justify-items-center">Team</div>
-	<div class="col-span-2 grid content-center justify-items-center">Country</div>
-	<div class="col-span-2 grid content-center justify-items-center">Player Data</div>
+	<div class="col-span-2 grid content-center justify-items-center">
+		<a href="/home" class="">
+			<Icon
+				src={Home}
+				size="40"
+				class="cursor-pointer hover:h-[50px] hover:w-[50px]"
+			/>
+		</a>
+	</div>
+	<div class="col-span-2 grid content-center justify-items-center">
+		<a href="/teams">
+			<Icon src={UserGroup} size="40" class="cursor-pointer hover:h-[50px] hover:w-[50px]" />
+		</a>
+	</div>
+	<div class="col-span-2 grid content-center justify-items-center">
+        <a href="/countriesd">
+		    <Icon src={Flag} size="40" class="cursor-pointer hover:h-[50px] hover:w-[50px]" />
+        </a>
+	</div>
+	<div class="col-span-2 grid content-center justify-items-center">
+        <a href="/data">
+			<Icon src={TableCells} size="40" class="cursor-pointer hover:h-[50px] hover:w-[50px]" />
+		</a>
+	</div>
+	<div class="col-span-2 grid content-center justify-items-end">
+		<img src="images/noRoomForRacism.png" class="w-28 pr-4" alt="noRoomForRacism" />
+	</div>
 </div>
--- FE_premier-zone/src/routes/+page.svelte
+++ FE_premier-zone/src/routes/+page.svelte
@@ -1,7 +1,4 @@
-<script lang="ts">
-	import Header from '../components/Header.svelte';
-</script>
 
 
 
-<Header />
+
