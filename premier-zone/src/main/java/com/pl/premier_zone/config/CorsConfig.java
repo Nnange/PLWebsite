6cb2c0a3fb9cffded18055706123965c864780e0
Nnange <awong.nnange@yahoo.com>
2025-03-26 14:41:19 +0100

    CORS bugfix

---
+++ premier-zone/src/main/java/com/pl/premier_zone/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.pl.premier_zone.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allows CORS for all endpoints
+                .allowedOrigins("http://192.168.2.90:3000", "http://localhost:5173") // the frontend origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true); // Allow cookies/auth if needed
+    }
+}
