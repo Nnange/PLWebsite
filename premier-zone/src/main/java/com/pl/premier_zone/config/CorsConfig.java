a0c7ca7d9a9c3bfe3e0794aa9ba25c8eca7c4b9b
Nnange <awong.nnange@yahoo.com>
2025-04-03 22:34:13 +0200

    adjusted Cors Policy

--- premier-zone/src/main/java/com/pl/premier_zone/config/CorsConfig.java
+++ premier-zone/src/main/java/com/pl/premier_zone/config/CorsConfig.java
@@ -9,7 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allows CORS for all endpoints
-                .allowedOrigins("http://192.168.2.90:3000", "http://localhost:5173") // the frontend origins
+                .allowedOrigins("http://192.168.2.90:3000", "http://localhost:5173"
+                        ,"https://epl.nnangedev.duckdns.org/") // the frontend origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allowed HTTP methods
                 .allowedHeaders("*")  // Allow all headers
                 .allowCredentials(true); // Allow cookies/auth if needed
